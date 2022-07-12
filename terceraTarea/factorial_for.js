d1b8808eb755619cd206e4222a0f384488bf3b04
LuijoO <Luijohm@gmail.com>
2022-07-12 12:36:44 +0200

    tercera tarea

---
+++ terceraTarea/factorial_for.js
@@ -0,0 +1,8 @@
+let num = 10
+let resultado = 1
+
+for (let i = 1; i <= num; i++) {
+  resultado=resultado*i;
+}
+
+console.log(resultado)
---
+++ terceraTarea/factorial_while.js
@@ -0,0 +1,8 @@
+let num = 10
+let num2 = 1
+
+while(num > 1) {
+  num2=num2*num;
+  num=num-1;
+  console.log(num2)
+}
