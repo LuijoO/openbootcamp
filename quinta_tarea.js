544940aa9df410c22a162a9fc96daebe27a875fd
LuijoO <Luijohm@gmail.com>
2022-07-14 17:17:34 +0200

    quinta tarea

---
+++ quinta_tarea.js
@@ -0,0 +1,17 @@
+let altura = 177
+console.log(altura)
+
+let alturaMts = 1.77
+console.log(alturaMts)
+
+let peso = 105
+console.log(peso)
+
+let alturaRedondeada = alturaMts.toFixed(1);
+console.log(alturaRedondeada)
+
+let pesoKilg = peso.toFixed(2);
+console.log(pesoKilg)
+
+let max_valor_js = Number.MAX_VALUE + 1;
+console.log(max_valor_js === Number.MAX_VALUE)
